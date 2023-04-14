66ce85103e1ec9922f3eb111d9bb5d0a2d36b347
Errol Lee <62831245+errolleesw@users.noreply.github.com>
2023-04-14 22:23:28 +1000

    update flight index view

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,11 @@ class Flight < ApplicationRecord
   belongs_to :airline
   belongs_to :origin, class_name: 'Airport'
   belongs_to :destination, class_name: 'Airport'
+
+  delegate :code, to: :origin, prefix: true
+  delegate :name, to: :origin, prefix: true
+  delegate :code, to: :destination, prefix: true
+  delegate :name, to: :destination, prefix: true
+  delegate :code, to: :airline, prefix: true
+  delegate :name, to: :airline, prefix: true
 end
